18f6887ad19a668bf1cfd91535f6a94c56d2f178
PaoloDC <PaoloDC@users.noreply.github.com>
2018-01-17 14:04:25 +0100

    inserisci attività v2

--- app/src/main/java/com/example/paolo/agendhelp/InserisciActivity.java
+++ app/src/main/java/com/example/paolo/agendhelp/InserisciActivity.java
@@ -1,25 +1,111 @@
 package com.example.paolo.agendhelp;
 
-import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 /**
  * Created by Paolo2 on 17/01/2018.
  */
 
-public class InserisciActivity extends Activity {
+public class InserisciActivity extends AppCompatActivity {
+    //costanti
+    public static final String DEBUG = "DEBUG";
+
+    //riferimenti al layout
+    private TextView tvAvviso;
+    private TextView tvData;
+    private TextView tvImportanza;
+    private TextView tvOra;
+    private TextView tvRipetizione;
+    private TextView tvSuoneria;
+    private EditText etNomeAttivita;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inserisci_layout);
 
+        tvAvviso = findViewById(R.id.tvAvviso);
+        tvData = findViewById(R.id.tvData);
+        tvImportanza = findViewById(R.id.tvImportanza);
+        tvOra = findViewById(R.id.tvOra);
+        tvRipetizione = findViewById(R.id.tvRipetizione);
+        tvSuoneria = findViewById(R.id.tvSuoneria);
+        etNomeAttivita = findViewById(R.id.etNomeAttivita);
+
     }
 
-    public void clickBackButton(View view) {
+    public void clickBack(View view) {
         finish();
     }
 
+
+    public void clickData(View v) {
+
+        Calendar calendar = Calendar.getInstance();
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int anno, int mese, int giorno) {
+                        mese++;
+                        String data = giorno + "/" + mese + "/" + anno;
+                        tvData.setText(data);
+                    }
+
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
+    }
+
+    public void clickOra(View v) {
+
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int ore, int minuti) {
+                        tvOra.setText(ore + ":" + minuti);
+                    }
+                },
+                Calendar.HOUR_OF_DAY,
+                Calendar.MINUTE,
+                true
+        );
+        timePickerDialog.show();
+    }
+
+    public void clickRipetizione(View view) {
+
+        
+    }
+
+    public void clickImportanza(View view) {
+
+    }
+
+    public void clickSuoneria(View view) {
+
+    }
+
+    public void clickAvviso(View view) {
+
+    }
+
     public void clickInserisciAttivita(View view) {
+
     }
 }
