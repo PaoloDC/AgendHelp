7a0d48b0888cd5313fe904aee2f6ead23721a2e1
PaoloDC <PaoloDC@users.noreply.github.com>
2018-01-17 14:33:37 +0100

    inserisci attività v2.1

--- app/src/main/java/com/example/paolo/agendhelp/InserisciActivity.java
+++ app/src/main/java/com/example/paolo/agendhelp/InserisciActivity.java
@@ -1,9 +1,12 @@
 package com.example.paolo.agendhelp;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -19,6 +22,13 @@ import java.util.Calendar;
 public class InserisciActivity extends AppCompatActivity {
     //costanti
     public static final String DEBUG = "DEBUG";
+    final String[] scelteRipetizione = {
+            " Una volta al giorno",
+            " Due volte al giorno",
+            " Una volta la settimana",
+            " Una volta al mese"
+    };
+
 
     //riferimenti al layout
     private TextView tvAvviso;
@@ -29,6 +39,12 @@ public class InserisciActivity extends AppCompatActivity {
     private TextView tvSuoneria;
     private EditText etNomeAttivita;
 
+    private String data;
+    private String ora;
+    private String ripetizione;
+    private boolean importanza;
+    private boolean suoneria;
+    private String avviso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +77,7 @@ public class InserisciActivity extends AppCompatActivity {
                     @Override
                     public void onDateSet(DatePicker datePicker, int anno, int mese, int giorno) {
                         mese++;
-                        String data = giorno + "/" + mese + "/" + anno;
+                        data = giorno + "/" + mese + "/" + anno;
                         tvData.setText(data);
                     }
 
@@ -78,7 +94,8 @@ public class InserisciActivity extends AppCompatActivity {
                 new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int ore, int minuti) {
-                        tvOra.setText(ore + ":" + minuti);
+                        ora = ore + ":" + minuti;
+                        tvOra.setText(ora);
                     }
                 },
                 Calendar.HOUR_OF_DAY,
@@ -90,15 +107,91 @@ public class InserisciActivity extends AppCompatActivity {
 
     public void clickRipetizione(View view) {
 
-        
+        // Creating and Building the Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Ripetizione");
+        builder.setSingleChoiceItems(scelteRipetizione, 1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                ripetizione = scelteRipetizione[item];
+                Log.d(DEBUG, "Scelta: " + scelteRipetizione[item]);
+            }
+        });
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tvRipetizione.setText(ripetizione);
+            }
+        });
+
+        builder.create().show();
+
     }
 
     public void clickImportanza(View view) {
 
+        final String[] scelte = {
+                "Alta importanza",
+                "Bassa importanza"
+        };
+
+        // Creating and Building the Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Importanza");
+        builder.setSingleChoiceItems(scelte, 1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                if (item == 0)
+                    importanza = true;
+                else
+                    importanza = false;
+
+            }
+        });
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (importanza)
+                    tvImportanza.setText(scelte[0]);
+                else
+                    tvImportanza.setText(scelte[1]);
+            }
+        });
+
+        builder.create().show();
     }
 
     public void clickSuoneria(View view) {
+        final String[] scelte = {
+                "Suoneria attivata",
+                "Suoneria disattivata"
+        };
 
+        // Creating and Building the Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Suoneria");
+        builder.setSingleChoiceItems(scelte, 1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (item == 0)
+                    suoneria = true;
+                else
+                    suoneria = false;
+
+            }
+        });
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (suoneria)
+                    tvSuoneria.setText(scelte[0]);
+                else
+                    tvSuoneria.setText(scelte[1]);
+            }
+        });
+
+        builder.create().show();
     }
 
     public void clickAvviso(View view) {
